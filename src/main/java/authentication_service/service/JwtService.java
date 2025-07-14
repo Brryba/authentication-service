0ef243ea1533b6033f3780373b3480141572925d
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-07-14 13:58:13 +0300

    user credentials database added

---
+++ src/main/java/authentication_service/service/JwtService.java
@@ -0,0 +1,8 @@
+package authentication_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+}
--- src/test/java/authentication_service/AuthenticationServiceApplicationTests.java
+++ src/test/java/authentication_service/AuthenticationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package innowise.authentificationservice;
+package authentication_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthentificationServiceApplicationTests {
+class AuthenticationServiceApplicationTests {
 
     @Test
     void contextLoads() {
