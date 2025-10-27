eed0cc3b7e3ff71e6f249fe02f8e7ba4be8b27f7
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-10-27 19:02:01 +0300

    ACCESS_TOKEN_JWT_SECRET name fixed

--- src/test/java/authentication_service/unit/AuthControllerTest.java
+++ src/test/java/authentication_service/unit/AuthControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         AuthController.class,
         GlobalExceptionHandler.class
 })
+@ActiveProfiles("test")
 public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
